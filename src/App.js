0f2b4c65119daa277d7e58afd64bbc982633a69e
Richard Young <richardyoung@Richards-MacBook-Pro.local>
2020-08-08 12:16:41 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Main = styled.div `
+  margin: 0px auto;
+  padding: 20px 30px;
+`;
+
+const Videosection = styled.section `
+ display: grid;
+ grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+ grid-gap: 2rem;
+`;
+
+const YearHeader = styled.div `
+  width: 100%;
+
+`;
+
+
+const Videocontainer = styled.div `
+  padding: 10px 20px;
+  border: 1px solid #b2b2b2;
+  place-self: center;
+`;
+
+
+const Video = styled.video `
+  width: 300px;
+`;
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main>
+    <YearHeader>
+      <h3>2017 - Radical Ritual</h3>
+    </YearHeader>
+      <Videosection>
+        <Videocontainer>
+          <h3>Hunter's Lodge</h3>
+          <p>Enjoying the freaky eclipse shadows.</p>
+          <Video controls>
+          <source src="https://www.dropbox.com/s/4gzzenq7tlv2ca8/IMG_1008.MOV?raw=1" type="video/mp4" />
+          </Video>
+        </Videocontainer>
+
+        <Videocontainer>
+          <h3>Hunters' Lodge</h3>
+          <p>It's a conspiracy...</p>
+          <Video controls>
+          <source src="https://www.dropbox.com/s/6ryqc2zlhrn1z3x/IMG_1013.MOV?raw=1" type="video/mp4" />
+          </Video>
+        </Videocontainer>
+
+        <Videocontainer>
+          <h3>2017 - Hunters' Lodge</h3>
+          <p>Enjoying the freaky eclipse shadows.</p>
+          <Video controls>
+          <source src="https://www.dropbox.com/s/4gzzenq7tlv2ca8/IMG_1008.MOV?raw=1" type="video/mp4" />
+          </Video>
+        </Videocontainer>
+
+        <Videocontainer>
+          <h3>2017 - Hunters' Lodge</h3>
+          <p>Enjoying the freaky eclipse shadows.</p>
+          <Video controls>
+          <source src="https://www.dropbox.com/s/4gzzenq7tlv2ca8/IMG_1008.MOV?raw=1" type="video/mp4" />
+          </Video>
+        </Videocontainer>
+
+        <Videocontainer>
+          <h3>2017 - Hunters' Lodge</h3>
+          <p>Enjoying the freaky eclipse shadows.</p>
+          <Video controls>
+          <source src="https://www.dropbox.com/s/4gzzenq7tlv2ca8/IMG_1008.MOV?raw=1" type="video/mp4" />
+          </Video>
+        </Videocontainer>
+
+        <Videocontainer>
+          <h3>2017 - Hunters' Lodge</h3>
+          <p>Enjoying the freaky eclipse shadows.</p>
+          <Video controls>
+          <source src="https://www.dropbox.com/s/4gzzenq7tlv2ca8/IMG_1008.MOV?raw=1" type="video/mp4" />
+          </Video>
+        </Videocontainer>
+
+        <Videocontainer>
+          <h3>2017 - Hunters' Lodge</h3>
+          <p>Enjoying the freaky eclipse shadows.</p>
+          <Video controls>
+          <source src="https://www.dropbox.com/s/4gzzenq7tlv2ca8/IMG_1008.MOV?raw=1" type="video/mp4" />
+          </Video>
+        </Videocontainer>
+      </Videosection>
+    </Main>
   );
 }
 
